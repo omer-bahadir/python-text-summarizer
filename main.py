332696c24c876122334b11c9d15ccbb68b5c202f
omer bahadir <omerbahadir2006@gmail.com>
2022-11-15 00:56:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ import numpy as np
 import networkx as nx
 import random
 
+correct_words = ["identity", "government", "preserve", "groups"]
+weight = random.random()
+bias = 3
+
 def read_text(file_name):
     sentences = []
     
     f_data = open(file_name, 'rb').read().decode(encoding='utf-8')
-    f_data = [x for x in f_data if x != '\n']
-    f_data = [x.replace('\n',' ') for x in f_data]
+    f_data = [x for x in f_data if x != '\n'] # it should remove any break present
+    f_data = [x.replace('\n',' ') for x in f_data] #this would remove that end of line
     f_data = ''.join(f_data) 
-    text = f_data.split('. ') 
+    article = f_data.split('. ') 
     
-    for sentence in text:
+    for sentence in article:
         sentences.append(sentence.replace("^[a-zA-Z0-9!@#$&()-`+,/\"]", " ").split(" "))
     
     return sentences
 
+# stop words are words like “a”, “the”, “is”, “are” and etc
 def sentence_similarity(sent1, sent2, stopwords=None):
-    # stop words are words like “a”, “the”, “is”, “are” and etc
     if stopwords is None: # if there are no stop words, then there are no stop words
         stopwords=[]
     
@@ -57,29 +61,30 @@ def sim_matrix(sentences,stop_words):
     
     return similarity_matrix
 
-def generate_summary(fileN, leng=1):
+def generate_summary(file_name,top_n=5):
     stop_words=stopwords.words('english')
     summarize_text=[]
-        
+    
     # split text
-    sentences =  read_text(fileN)
+    sentences =  read_text(file_name)
 
     # generates the similarity matrix across sentences
-    sentence_similarity_martix = sim_matrix(sentences, stop_words)  
+    sentence_similarity_martix = sim_matrix(sentences, stop_words)
 
     # ranks sentences in similarity martix
     sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
     scores = nx.pagerank(sentence_similarity_graph)
 
     # sorts the rank and pick top sentences
+    
     ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
-        
+    
+    
     #print("Indexes of top ranked_sentence are ", ranked_sentence)   
-        
-    for i in range(leng):
+    
+    for i in range(top_n):
         summarize_text.append(" ".join(ranked_sentence[i][1]))
-        
-    return summarize_text
-    #print("Summary: \n",". ".join(summarize_text))
+    
+    print("Summary: \n",". ".join(summarize_text))
 
-generate_summary("essay.txt")
+generate_summary("essay.txt",5)
